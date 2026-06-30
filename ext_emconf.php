3b5c6917ddd8942a1f137b5e250f1d8f1314b81c
Karsten Nowak <nowak@undkonsorten.com>
2026-06-30 17:31:46 +0200

    [!!!][TASK] Start refactoring email templates to v14 features
    Drop TYPO3 9,10,11,12,13 support
    
    Related: #29705

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
 	'version' => '1.1.0',
 	'constraints' => [
 		'depends' => [
-			'typo3' => '9.5.0 - 14.3.99',
+			'typo3' => '14.0.0 - 14.3.99',
 			'html_mail_utility' => '1.0.0 - 0.0.0',
         ],
     ],
